2e468f9953020e2737e948a72326232f1e0d386b
Yi-Ru Lan <a78945612385238@gmail.com>
2022-11-04 02:20:37 +0800

    Init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li v-for="(path, index) in paths" :key="index">
+      <button @click.prevent="$router.push(path)">{{ path }}</button>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+const paths = ['/', '/users', '/users/a', '/users/b', '/users/c'];
+</script>
+
+<style scoped>
+button {
+  cursor: pointer;
+}
+</style>
---
+++ src/pages/users/[id].vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>User:{{ route.params.id }} page</h1>
+</template>
+
+<script setup lang="ts">
+import { watch } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute('/users/[id]');
+
+watch(
+  () => route.params.id,
+  (newId) => console.log(newId),
+);
+</script>
+
+<style scoped></style>
